f6abd56a4a155ca694ddcaf35f5f3bef17d3f7b6
JeremyVu <vulong1207@gmail.com>
2023-10-04 20:01:29 +0700

    them san pham perfect

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,141 +1,157 @@
 @extends('admin.layout.master')
 @section('content')
-    <form action="{{ route('admin.product.store') }}" method="post">
+    <style>
+        .mr-1 {
+            display: table-cell;
+        }
+
+        label {
+            margin-left: 5px;
+            margin-bottom: 1rem;
+            display: block;
+        }
+    </style>
+    <form action="{{ route('admin.product.store') }}" method="post" style="display: table">
         @csrf
         <div class="form-group">
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> Tên sản phẩm: </label>
                 <label>
                     <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+                    @if ($errors->has('name'))
+                        <span class="error" style="color: red; font-size: 12px;">
+                        {{ $errors->first('name') }}
+                    </span>
+                    @endif
                 </label>
-                @if ($errors->has('name'))
-                    <span class="error" style="color: red; font-size: 12px;">
-                             {{ $errors->first('name') }}
-                        </span>
-                @endif
             </div>
 
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> Giá sản phẩm: </label>
                 <label>
                     <input type="number" min="0" name="price" value="{{ old('price') }}" class="form-control">
-                </label>
-                @if ($errors->has('price'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('price'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('price') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> Số lượng: </label>
                 <label>
                     <input type="number" min="0" name="quantity" class="form-control" value="{{ old('quantity') }}">
-                </label>
-                @if ($errors->has('quantity'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('quantity'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('quantity') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
             <div hidden>
                 <label class="mr-1"> Slug: </label>
                 <label>
                     <input disabled type="text" name="slug" class="form-control">
-                </label>
-                @if ($errors->has('slug'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('slug'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('slug') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> Ảnh: </label>
                 <label>
-                    <input type="file" name="image" value="{{ old('image') }}" class="form-control">
-                </label>
-                @if ($errors->has('image'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    <input type="file" name="image" value="{{ old('image') }}" class="form-control-file">
+                    @if ($errors->has('image'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('image') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
 
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> SKU: </label>
                 <label>
                     <input type="text" name="sku" value="{{ old('sku') }}" class="form-control">
-                </label>
-                @if ($errors->has('sku'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('sku'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('sku') }}
                     </span>
-                @endif
-            </div>
-
-
-            <div>
-                <label class="mr-1"> Chi tiết sản phẩm: </label>
-                <label>
-                    <input type="text" name="detail_product" value="{{ old('detail_product') }}" class="form-control">
+                    @endif
                 </label>
-                @if ($errors->has('detail_product'))
-                    <span class="error" style="color: red; font-size: 12px;">
-                        {{ $errors->first('detail_product') }}
-                    </span>
-                @endif
             </div>
 
-            <div>
+            <div style="display: table-row">
                 <label class="mr-1"> Mô tả: </label>
                 <label>
-                <textarea type="text" name="description" placeholder="{{ old('description') }}" class="form-control">
-                </textarea>
-                </label>
-                @if ($errors->has('description'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    <input type="text" name="description" value="{{ old('description') }}" class="form-control">
+                    @if ($errors->has('description'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('description') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
-            <div>
-                <label class="mr-1"> Thương hiệu : </label>
+            <div style="display: table-row">
+                <label class="mr-1"> Chi tiết sản phẩm: </label>
+                <label>
+                    <textarea type="text" name="detail_product" placeholder="{{ old('detail_product') }}"
+                              class="form-control">
+                    </textarea>
+                    @if ($errors->has('detail_product'))
+                        <span class="error" style="color: red; font-size: 12px;">
+                        {{ $errors->first('detail_product') }}
+                    </span>
+                    @endif
+                </label>
+
+            </div>
+
+
+            <div style="display: table-row">
+                <label class="mr-1"> Thương hiệu: </label>
                 <label>
                     <select name="brand_id" class="form-control select">
+                        <option value="" selected>Chọn thương hiệu</option>
                         @foreach ($brands as $brand)
                             <option value="{{ $brand->id }}">
                                 {{ $brand->name }}
                             </option>
                         @endforeach
                     </select>
-                </label>
-                @if ($errors->has('brand_id'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('brand_id'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('brand_id') }}
                     </span>
-                @endif
+                    @endif
+                </label>
             </div>
 
-            <div>
-                <label class="mr-1"> Danh mục : </label>
+            <div style="display: table-row">
+                <label class="mr-1"> Danh mục: </label>
                 <label>
                     <select name="category_id" class="form-control select">
+                        <option value="" selected> Chọn danh mục</option>
                         @foreach ($categories as $category )
                             <option value="{{ $category->id }}">
                                 {{ $category->name }}
                             </option>
                         @endforeach
                     </select>
-                </label>
-                @if ($errors->has('category_id'))
-                    <span class="error" style="color: red; font-size: 12px;">
+                    @if ($errors->has('category_id'))
+                        <span class="error" style="color: red; font-size: 12px;">
                         {{ $errors->first('category_id') }}
                     </span>
-                @endif
+                    @endif
+                </label>
+
             </div>
             <br>
 
